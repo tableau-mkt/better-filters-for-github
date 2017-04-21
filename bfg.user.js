93e544e86dc96e337e284c35cc21bef20e7b7214
Joel <jtwalters@gmail.com>
2017-04-21 11:42:43 -0700

    Bump version

--- bfg.user.js
+++ bfg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Filters for GitHub
 // @namespace    http://joelwalters.com/
-// @version      1.0.3
+// @version      1.0.4
 // @description  Provides filters for GitHub Projects.
 // @author       Joel Walters
 // @match        https://github.com/*
