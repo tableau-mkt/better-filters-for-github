b8531fb50fa8d31ab1d53824adb0e3d61b930a24
BJ Miller <bjmiller121@gmail.com>
2017-04-25 11:59:59 -0700

    Adding an info icon with a tooltip for more info about the plugin.

--- bfg.user.js
+++ bfg.user.js
@@ -19,6 +19,7 @@
     var $filterAssignee = $('<select class="ght-filter form-select select-sm"><option value="">- Filter by Assignee -</option></select>');
     var $filterClear = $('<a href="#" class="btn btn-sm"><svg class="octicon octicon-x" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"></path></svg> clear filters</a>');
     var $toggleLabels = $('<div><label><input type="checkbox" id="ghtToggleLabels" name="ghtToggleLabels"> Show Labels</label></div>');
+    var $infoTooltip = $('<div class="ght-item left pt-1"><span class="tooltipped tooltipped-e" aria-label="Filters provided by Better Filters for Github (click for more)"><a href="https://github.com/tableau-mkt/better-filters-for-github" class="link-gray-dark" target="_blank"><svg class="octicon octicon-info" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M6.3 5.69a.942.942 0 0 1-.28-.7c0-.28.09-.52.28-.7.19-.18.42-.28.7-.28.28 0 .52.09.7.28.18.19.28.42.28.7 0 .28-.09.52-.28.7a1 1 0 0 1-.7.3c-.28 0-.52-.11-.7-.3zM8 7.99c-.02-.25-.11-.48-.31-.69-.2-.19-.42-.3-.69-.31H6c-.27.02-.48.13-.69.31-.2.2-.3.44-.31.69h1v3c.02.27.11.5.31.69.2.2.42.31.69.31h1c.27 0 .48-.11.69-.31.2-.19.3-.42.31-.69H8V7.98v.01zM7 2.3c-3.14 0-5.7 2.54-5.7 5.68 0 3.14 2.56 5.7 5.7 5.7s5.7-2.55 5.7-5.7c0-3.15-2.56-5.69-5.7-5.69v.01zM7 .98c3.86 0 7 3.14 7 7s-3.14 7-7 7-7-3.12-7-7 3.14-7 7-7z"></path></svg></a></span></div>');
     var $controls = $('<div class="ght-controls clearfix pt-3"></div>');
     var counts = {};
     var values = {};
@@ -137,6 +138,7 @@
     .append($filterLabel)
     .append($filterAssignee)
     .append($filterClear)
+    .append($infoTooltip)
     .append($toggleLabels);
 
     $controls.find('> *').wrap('<div class="ght-item left">').last().parent().addClass('right').removeClass('left');
